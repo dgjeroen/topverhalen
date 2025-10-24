e855935fffa8a8c011f0ec0427c0c5871e284eee
Jeroen Kuitert <j.kuitert@gelderlander.nl>
2025-10-24 09:25:16 +0200

    Add static build config

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
 	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			runtime: 'nodejs20.x'
 		}),
+
 		alias: {
 			'$lib': 'src/lib'
 		}
 	}
 };
+
+export default config;
